dcf49938856d0f7a5f24a15f40850ddba201273a
STAM0283 <aminestambouli00780@gemail.com>
2020-07-17 21:07:35 +0200

    react 5 resolved

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,19 +3,30 @@ import React from 'react';
 import './chat-app.css';
 
 
-function Contact(props){
-    return (
+class Contact extends React.Component{
+   constructor(props){
+      super(props)
+      this.state = {online : props.online}   }
+   render(){
+      return (
     <div className = "Contact">
-       <img  className = "avatar" src = {props.avatar} alt = {props.name}/>
+       <img  className = "avatar" src = {this.props.avatar} alt = {this.props.name}/>
 
-       <div className = "status">
-          <h2 className = "name">{props.name}</h2>
-          <p className = {props.status ? "status-online" : "status-offline"}/>
-          <p className = "status-text">{props.status? "Online" : "Offline"}</p>
+       <div className = "status" onClick = {event => {
+             const newOnline = !this.state.online;
+             this.setState({online : newOnline})
+          }
+         }>
+          <h2 className = "name">{this.props.name}</h2>
+          <div className = {this.state.online ? "status-online" : "status-offline"}
+          />
+          <div className = "status-text">{this.state.online? "Online" : "Offline"}</div>
        </div> 
     </div>
 
     )
+   }
+    
 
 }
 Contact.propTypes = {
